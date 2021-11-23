f3e9102c5a4a67fe4c7509831d5504902a27e084
JoselinPaz <njrodriguezp@unah.hn>
2021-11-22 22:18:51 -0600

    Actualizacion ruta controller Pedidos

--- PEDIDOS/controller/pedidos.php
+++ PEDIDOS/controller/pedidos.php
@@ -11,8 +11,8 @@ if ($_SERVER['REQUEST_METHOD'] === 'OPTIONS') {
    header('Access-Control-Allow-Origin: *');  
    header('Content-Type: application/json');
 
-     require_once("../config/conexion.php");
-     require_once("../models/Pedidos.php");
+     require_once("C:/xampp/htdocs/G5_19/config/conexion.php");
+     require_once("C:/xampp/htdocs/G5_19/PEDIDOS/models/Pedidos.php");
      $pedidos = new pedidos();
 
      $body = json_decode(file_get_contents("php://input"), true);
